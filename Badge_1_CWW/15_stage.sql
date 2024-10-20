52b933142f8d5f004dc3029a4d956bf8128f48cb
Francois-Xavier Fabre <fxfabre@users.noreply.github.com>
2024-10-20 08:08:32 +0200

    Snowflake badge 1

--- Badge_1_CWW/15_stage.sql
+++ Badge_1_CWW/15_stage.sql
@@ -1,7 +1,21 @@
+use database util_db;
+use schema public;
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW10' as step
+  ,( select count(*)
+    from UTIL_DB.INFORMATION_SCHEMA.stages
+    where stage_name='MY_INTERNAL_STAGE'
+    and stage_type is null) as actual
+  , 1 as expected
+  , 'Internal stage created' as description
+ );
+
 use database GARDEN_PLANTS;
 use schema VEGGIES;
 create or replace table vegetable_details_soil_type
 (plant_name varchar(25), soil_type number(1,0));
+
 
 create file format garden_plants.veggies.PIPECOLSEP_ONEHEADROW
     type = 'CSV'--csv is used for any flat file (tsv, pipe-separated, etc)
@@ -13,6 +27,19 @@ copy into vegetable_details_soil_type
 from @util_db.public.my_internal_stage
 files = ( 'VEG_NAME_TO_SOIL_TYPE_PIPE.txt')
 file_format = ( format_name=GARDEN_PLANTS.VEGGIES.PIPECOLSEP_ONEHEADROW );
+
+
+use database util_db;
+use schema public;
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+  SELECT 'DWW11' as step
+  ,( select row_count
+    from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES
+    where table_name = 'VEGETABLE_DETAILS_SOIL_TYPE') as actual
+  , 42 as expected
+  , 'Veg Det Soil Type Count' as description
+ );
+
 
 use database GARDEN_PLANTS;
 use schema VEGGIES;
@@ -60,3 +87,40 @@ copy into GARDEN_PLANTS.VEGGIES.VEGETABLE_DETAILS_PLANT_HEIGHT
 from @util_db.public.my_internal_stage
 files = ( 'veg_plant_height.csv')
 file_format = ( format_name=GARDEN_PLANTS.VEGGIES.CSV_COMMA_ONEHEADROW );
+
+
+use database util_db;
+use schema public;
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+      SELECT 'DWW12' as step
+      ,( select row_count
+        from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES
+        where table_name = 'VEGETABLE_DETAILS_PLANT_HEIGHT') as actual
+      , 41 as expected
+      , 'Veg Detail Plant Height Count' as description
+);
+
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+     SELECT 'DWW13' as step
+     ,( select row_count
+       from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES
+       where table_name = 'LU_SOIL_TYPE') as actual
+     , 8 as expected
+     ,'Soil Type Look Up Table' as description
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
